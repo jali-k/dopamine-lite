b82378a313280a12681ad5f57207b23acf271507
jali-k <e19210@eng.pdn.ac.lk>
2025-02-07 16:06:20 +0530

    Configure Routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,41 +13,51 @@ import AdminButton from "./components/AdminButton";
 import PDFFileView from "./pages/PDFFileView";
 import PDFPage from "./pages/PDFPage";
 import EditTutorialPage from "./pages/EditTutorialPage"; // New import
+import BrowserDetection from "./components/BrowserDetectionComponent";
+import { ThemeProvider, createTheme } from '@mui/material';
+import KeyPressTracker from "./components/KeyPressTracker";
+
+const theme = createTheme();
 
 export default function App() {
   return (
-    <Routes>
-      <Route
-        element={
-          <>
-            <AdminButton />
-            <Outlet />
-          </>
-        }
-      >
-        {/* Student routes */}
-        <Route path="/" element={<FVStuPage />} />
-        <Route path="/video" element={<FVStuPage />} />
-        <Route path="/pdf" element={<FVStuPage />} />
-        <Route path="/video/:fname" element={<StuFileView />} />
-        <Route path="/video/:fname/:lname" element={<VideoPage />} />
-        <Route path="/pdf/:fname" element={<PDFFileView />} />
-        <Route path="/pdf/:fname/:lname" element={<PDFPage />} />
+    <ThemeProvider theme={theme}>
+    <BrowserDetection>
+    <KeyPressTracker />
+      <Routes>
+        <Route
+          element={
+            <>
+              <AdminButton />
+              <Outlet />
+            </>
+          }
+        >
+          {/* Student routes */}
+          <Route path="/" element={<FVStuPage />} />
+          <Route path="/video" element={<FVStuPage />} />
+          <Route path="/pdf" element={<FVStuPage />} />
+          <Route path="/video/:fname" element={<StuFileView />} />
+          <Route path="/video/:fname/:lname" element={<VideoPage />} />
+          <Route path="/pdf/:fname" element={<PDFFileView />} />
+          <Route path="/pdf/:fname/:lname" element={<PDFPage />} />
 
-        {/* Admin routes */}
-        <Route path="/admin" element={<FVAdPage />} />
-        <Route path="/admin/video" element={<FVAdPage />} />
-        <Route path="/admin/video/:fname/add" element={<VideoUPPage />} />
-        <Route path="/admin/video/:fname" element={<AdmFileView />} />
-        <Route path="/admin/video/:fname/:lname" element={<ADVideoPage />} />
-        <Route path="/admin/video/:fname/edit/:tname" element={<EditTutorialPage />} />
-        <Route path="/admin/pdf" element={<FVAdPage />} /> 
-        <Route path="/admin/pdf/:fname/add" element={<PDFUploaderPage />} />
-        <Route path="/admin/pdf/:fname" element={<AdmPDFFileView />} />
-        <Route path="/admin/pdf/:fname/:lname" element={<PDFPage />} />
+          {/* Admin routes */}
+          <Route path="/admin" element={<FVAdPage />} />
+          <Route path="/admin/video" element={<FVAdPage />} />
+          <Route path="/admin/video/:fname/add" element={<VideoUPPage />} />
+          <Route path="/admin/video/:fname" element={<AdmFileView />} />
+          <Route path="/admin/video/:fname/:lname" element={<ADVideoPage />} />
+          <Route path="/admin/video/:fname/edit/:tname" element={<EditTutorialPage />} />
+          <Route path="/admin/pdf" element={<FVAdPage />} />
+          <Route path="/admin/pdf/:fname/add" element={<PDFUploaderPage />} />
+          <Route path="/admin/pdf/:fname" element={<AdmPDFFileView />} />
+          <Route path="/admin/pdf/:fname/:lname" element={<PDFPage />} />
 
-        <Route path="*" element={<Error404 />} />
-      </Route>
-    </Routes>
+          <Route path="*" element={<Error404 />} />
+        </Route>
+      </Routes>
+    </BrowserDetection>
+    </ThemeProvider>
   );
 }
